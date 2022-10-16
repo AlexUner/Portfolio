10e171105efbc3ba4f75c202d884dd019dc8be92
Alexandr Bondarenko <69047343+AlexUner@users.noreply.github.com>
2022-10-16 09:11:02 +0300

    Update procedure.asm

--- Assembler/procedure/procedure.asm
+++ Assembler/procedure/procedure.asm
@@ -15,9 +15,9 @@ option casemap: none
 		MsgBoxName    	db "MASM32 Prog",0
 		
 		InputBoxName    db "Line entry",0
-		InputDefBoxText db "Please enter a STRING no longer than 255 characters!",0
+		InputDefBoxText db "Please enter a STRING no longer than 50 characters!",0
 		Format1         db "The symbols are displayed:",0
-		Format2         db 13,"Character count: %lu",13,"Do you want to enter new data?",0
+		Format2         db 13,"Character count: %lu",13,"Enter new data?",0
 		
 		FirstString   	db 50  dup (0)
 		SecondString   	db 50  dup (0)
@@ -28,8 +28,8 @@ option casemap: none
 		mode            db 0
 		
 		ErrCaption      db "Error",0
-		ValueInputError db "You have not entered values", 13, "Do you want to re-enter?",0
-		DefLengthError  db "Âû ââåëè áîëüøå 50 ñèìâîëîâ!", 13, "Ïîâòîðèòü ââîä?",0		
+		ValueInputError db "You have not entered values!", 13, "Re-enter?",0
+		DefLengthError  db "You have entered more than 50 characters!", 13, "Re-enter?",0		
 		
 ;#########################################################################	
 	.code
@@ -38,40 +38,40 @@ include   \masm32\include\str.inc
 start:
 	mov    mode, 1
 	invoke InputBox, addr InputDefBoxText, addr InputBoxName, addr FirstString
-	test   eax, eax                 ; ïðîâåðêà íà ïóñòîòó
-	jz 	   errValueInput  			; íå ââåäåíî
-	cmp    eax, 50                  ; ïðîâåðêà íà äëèíó
-	ja     errDefLength	  			; áîëüøå 50
+	test   eax, eax                 ; empty check
+	jz 	   errValueInput  			; not entered
+	cmp    eax, 50                  ; length check
+	ja     errDefLength	  			; over 50
 s2:
 	mov    mode, 2
 	invoke InputBox, addr InputDefBoxText, addr InputBoxName, addr SecondString
-	test   eax, eax                 ; ïðîâåðêà íà ïóñòîòó
-	jz 	   errValueInput  			; íå ââåäåíî
-	cmp    eax, 50                  ; ïðîâåðêà íà äëèíó
-	ja     errDefLength	  			; áîëüøå 50
+	test   eax, eax                 
+	jz 	   errValueInput  			
+	cmp    eax, 50                  
+	ja     errDefLength	  			
 s3:	
 	mov    mode, 3
 	invoke InputBox, addr InputDefBoxText, addr InputBoxName, addr ThirdString
-	cmp    eax, 50                  ; ïðîâåðêà íà äëèíó
-	ja     errDefLength	  			; áîëüøå 50
+	cmp    eax, 50                  
+	ja     errDefLength	  			
 s4:
 	mov    mode, 4
 	invoke InputBox, addr InputDefBoxText, addr InputBoxName, addr FourthString
-	cmp    eax, 50                  ; ïðîâåðêà íà äëèíó
-	ja     errDefLength	  			; áîëüøå 50
+	cmp    eax, 50                  
+	ja     errDefLength	  			
 	
-	push   offset ResString			; 5 àðãóìåíò ðåçóëüòèðóþùèé
-	push   offset FirstString       ; 1 àðãóìåíò
-	push   offset SecondString		; 2 àðãóìåíò
-	push   offset ThirdString		; 3 àðãóìåíò
-	push   offset FourthString		; 4 àðãóìåíò
+	push   offset ResString			; 5 argument resultant
+	push   offset FirstString       ; 1 argument
+	push   offset SecondString		; 2 argument
+	push   offset ThirdString		; 3 argument
+	push   offset FourthString		; 4 argument
 	call   Concatenatio
 	
 	push   eax
 	
 	push   offset ValueLength
 	push   offset Format1
-	push   offset ResString			; ñîáèðàåì âûâîä
+	push   offset ResString			; putting together the output
 	push   offset Format2
 	push   0
 	call   Concatenatio
@@ -85,7 +85,7 @@ s4:
 	jz     start
 	jmp    exit	
 	
-errDefLength:						; îáðàáîòêà îøèáîê
+errDefLength:						; error handling
 	invoke MessageBox, NULL, addr DefLengthError, addr ErrCaption, MB_RETRYCANCEL + MB_ICONERROR
 	jmp    errRet
 	
@@ -93,7 +93,7 @@ errValueInput:
 	invoke MessageBox, NULL, addr ValueInputError, addr ErrCaption, MB_RETRYCANCEL + MB_ICONERROR	
 	jmp    errRet
 
-errRet:								; îáðàáîòêà âîçâðàòà
+errRet:								; return processing
 	cmp    EAX, 4
 	jz     defmode
 	jmp    exit
@@ -111,14 +111,3 @@ exit:
 	invoke ExitProcess, NULL
 	
 end start
-
-
-
-
-
-
-
-
-
-
-
